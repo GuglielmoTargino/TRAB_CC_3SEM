7aed7e1036241f911e0dabefb40566dd321f7e2f
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-05-30 21:39:42 -0300

    continua_POO
    Continuação da programação POO

---
+++ Exemplo3005/src/NotasService.java
@@ -0,0 +1,15 @@
+
+public class NotasService {
+
+    public static String verificarAprovacao(double nota, int nAulas, int nFaltas) {
+
+        double limiteFaltas;
+        limiteFaltas = nAulas * 25 / 100;
+        if (nota < 6 || nFaltas > limiteFaltas) {
+            return "Reprovado";
+        } else {
+            return "Aprovado";
+        }
+
+    }
+}
